1fac434c89052145ca3c5b39cdb48e9dbce2bc77
Soreikhudai <khudaisoreipam@gmail.com>
2023-02-22 22:45:31 +0530

    header created

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from 'react'
+import mealImage from '../../assets/hdmeal.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+const Header = (props) => {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h2> Daisy Meals </h2>
+               <HeaderCartButton/>
+            </header>
+            <div className={classes ['main-image']}>
+                <img src={mealImage} alt="your favourite order"/>
+            </div>
+        </Fragment>
+    )
+}
+export default Header;
