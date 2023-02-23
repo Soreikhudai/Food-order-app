d63cdbe903c2580f2d6be16bd47f271a03cd7f9d
Soreikhudai <khudaisoreipam@gmail.com>
2023-02-23 22:18:23 +0530

    cart enabled open and close

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>Daisy Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='The food of your choice!' />
